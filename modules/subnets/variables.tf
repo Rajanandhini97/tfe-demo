21be08b8eafe9c411f4df76a052c2440362a8aea
Rajanandhini97 <nrajanandhini97@gmail.com>
2023-11-05 09:22:25 -0500

    Modules for subnet and webserver

---
+++ modules/subnets/variables.tf
@@ -0,0 +1,13 @@
+variable "vpc_id" {
+  description = "VPC id"
+}
+
+variable "subnet_cidr_block" {
+    description = "Subnet CIDR block"
+}
+
+variable "availability_zone" {}
+
+variable "env_prefix" {}
+
+variable "default_route_table_id" {}
